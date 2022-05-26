0938b66d56c2cbe2a33eba06d3adae0c87f7513c
jcnpereira <casimiro@ipt.pt>
2022-05-26 23:28:47 +0100

    uso de Props

--- src/App.js
+++ src/App.js
@@ -5,11 +5,35 @@
 import React from "react";
 import Tabela from "./Tabela";
 import Tabela2 from "./Tabela2";
+import Tabela3 from "./Tabela3";
 
 
 // componente REACT
 class App extends React.Component {
   render() {
+
+    // criar uma lista de alunos
+    // PROPS (propriedades)
+    // são invariáveis
+    const alunos = [
+      {
+        nome: "João",
+        apelido: "Sousa",
+      },
+      {
+        nome: "Ana",
+        apelido: "Pires",
+      },
+      {
+        nome: "Mário",
+        apelido: "Silva",
+      },
+      {
+        nome: "Luís",
+        apelido: "Sousa",
+      },
+    ]
+
     return (
       <div className="container">
         <h1>Primeira Lista de Alunos</h1>
@@ -18,6 +42,13 @@ class App extends React.Component {
         <br /><br /><br />
         <h1>Segunda Lista de Alunos</h1>
         <Tabela2 />
+
+        <br /><br /><br />
+        <h1>Terceira Lista de Alunos</h1>
+        {/* dadosAlunos é um parâmetro de exportação,
+            usado para entregar o array com os dados dos alunos
+            ao componente Tabela3 */}
+        <Tabela3 dadosAlunosIN={alunos} />
       </div>
     )
   }
